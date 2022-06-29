99c9b001b2f38c1111edfa8ec86a811344f2e487
lynchzival <lynchzival@gmail.com>
2022-06-30 01:31:47 +0700

    remove secret key

--- school/settings.py
+++ school/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1imk36*52+i*efg+0f^yy1((mi=%u9*_)o#8@)&$!5sc540w9q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
